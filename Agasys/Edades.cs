de733aa2f58b61aa832fb5c81697640f183e0142
Jaime CR <jaimecoromero@outlook.com>
2022-12-14 00:22:15 -0600

    Edad

--- Agasys/Edades.cs
+++ Agasys/Edades.cs
@@ -9,24 +9,24 @@ namespace Agasys
     public class Edades
     {
 		public static void Mayores()
-		{
-            //int edad;
-            Random random = new Random();
-            //Console.WriteLine("Persona");
+		{  
+            
             for (int i = 0; i < 20; i++)
             {
-                if (ramdom >= 18)
+                Random random = new Random();
+                int dato = random.Next();
+                
+                if (dato >= 18)
                 {
-                    Console.WriteLine(random.Next(1, 20));
-                    Console.WriteLine("Es Mayor de Edad: ");
+                    Console.WriteLine(random.Next());
+                    Console.WriteLine("Es Mayor de Edad: " );
                 }
                 else 
                {
                     Console.WriteLine("Es Menor de Edad: ");
                 }
                 Console.ReadLine();
-            //}
-
+            }
 
         }
     }
